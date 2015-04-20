a24b146fa63733b7a92a38cee659746910fa513c
Niklas P <contact@niklasplessing.net>
2015-04-20 16:06:08 +0200

    update

--- content-home.php
+++ content-home.php
@@ -17,7 +17,7 @@
 			<h2><span>Demokratische Produktion</span></h2>
 			<p class="f20">y8x soll ein Unternehmen werden, das eine Platform mit Open Source Baupläne für Maschinen und Objekte 
 				zur Verfügung stellt.</p> 
-				<p class="f20">Nutzer_innen können Baupläne hochladen, runterladen, online bewerten und verbessern, 
+				<p class="f20">Nutzer_innen können Bauplänen hochladen, runterladen, online bewerten und verbessern, 
 				sowie entsprechende Bausätze bestellen. Die Einnahmen werden durch den Verkauf von Bausätzen, 
 				die alle benötigten Bauteile für einen Bauplan enthalten generiert. 
 			</div>
@@ -89,7 +89,7 @@
 		</div>
 		<hr>
 		<div class="col-md-12">
-		<h3 id="Produktion">Produktion</h3>
+		<h2 id="Produktion">Produktion</h2>
 		<p>Statt teure, bearbeitete, vorgefertigte Produkte zu kaufen, möchten wir dazu beitragen, dass es einfacher für Jedermensch 
 			wird, Produkte selbst zu entwerfen und zu bauen. Es gibt schon heute viele Kleinunternehmer_innen, 
 			die eine Laser-Schneidemaschine besitzen und damit den Zuschnitt von eigenen Entwürfen ermöglichen. 
@@ -106,54 +106,47 @@
 			</ul>
 		</div>
 		<hr>
-		<h3 id="Produkte">Produkte (beta)</h3>
+		<h2 id="Produkte">Produkte (beta)</h2>
 		<div class="row mt-lg">
+			<div class="col-md-12 mb-lg">
+				<p>Das Ziel ist es eine Platform zu schaffen, auf der Baupläne für viele verschiedene Dinge angesehen werden,
+					und die zugehörigen Bausätze direkt bestellt werden können. Jede_r soll Pläne hochladen können, die wir überprüfen, nachbauen
+					und falls geeignet zu unserem Sortiment hinzufügen. Bis die Infrastruktur dazu geschaffen wurde stellen wir euch
+					drei Produkte vor, an deren Plänen wir gerade arbeiten und deren Prototypen wir bereits testen.
+			</div>
 			<div class="col-md-4">
 				<img src="http://www.radfahrlust.de/images/lastenrad.jpg" class="auto">
-			</div>
-			<div class="col-md-8">
-				<h3>Lastenrad</h3>
+				<h3 class="mt-lg">Lastenrad</h3>
 				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
 					aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
 					Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
 					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
 					invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
 					accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-					sanctus est Lorem ipsum dolor sit amet.</p>
+					sanctus est Lorem ipsum dolor sit amet.</p>				
 			</div>
-		</div>
-		<div class="row mt-lg">
 			<div class="col-md-4">
 				<img src="http://www.duden.de/_media_/full/I/Ikosaeder-201020349442.jpg" class="auto">
-			</div>
-			<div class="col-md-8">
-				<h3>Gewächshaus</h3>
+				<h3 class="mt-lg">Gewächshaus</h3>
 				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
 					aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
 					Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
 					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-					invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-					accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-					sanctus est Lorem ipsum dolor sit amet.</p>
+					invidunt ut labore et dolore magna aliquyam erat</p>				
 			</div>
-		</div>
-		<div class="row mt-lg">
+		
 			<div class="col-md-4">
 				<img src="https://a248.e.akamai.net/f/248/9086/10h/manufactum.scene7.com/is/image/Manufactum/25991_01" class="auto">
-			</div>
-			<div class="col-md-8">
-				<h3>Ofen</h3>
+				<h3 class="mt-lg">Ofen</h3>
 				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
 					aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
 					Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-					invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-					accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-					sanctus est Lorem ipsum dolor sit amet.</p>
+					</p>				
 			</div>
-		</div>				
+		</div>					
+	
 		<hr>
-		<h3 id="Commons">Commons</h3>
+		<h2 id="Commons">Commons</h2>
 		<p>Gleichzeitig zur Möglichkeit der Fertigung von eigenen Plänen, wollen wir ein Archiv an frei zugänglichen Schnittmustern aufbauen, 
 			sodass jede Idee für alle zum Nachmachen zugänglich ist. Daher sollen Nutzer_innen beim hochladen Ihrer Baupläne die 
 			Möglichkeit haben, den Plan für alle sichtbar zu machen und so zur öffentlichen Produktion beizutragen</p>
